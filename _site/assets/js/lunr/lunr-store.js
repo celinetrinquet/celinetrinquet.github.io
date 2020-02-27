8e98834d2fb26de99e2c02ad30824784591f8118
Pedro SOARES <pedrosoares@MBP-de-Pedro.home>
2020-02-27 13:28:09 +0100

    git

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/portfolio/2/",
-        "teaser":"http://localhost:4000/assets/images/foo-bar-identity-th.jpg"},]
+        "teaser":"http://localhost:4000/assets/images/foo-bar-identity-th.jpg"},{
+        "title": "3",
+        "excerpt":"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/3/",
+        "teaser":null},]
